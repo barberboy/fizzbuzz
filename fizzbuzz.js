f319340b40bc9f0fab1dfd98c4626a9327b6929d
Ben Barber <barberboy@gmail.com>
2016-09-11 22:27:37 +0000

    Create a fizzbuzz function and call it multiple times
    This completes the extra credit assignment.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,13 +1,20 @@
-for (var i = 1; i <= 100; i++) {
-    var output = ''
-    if((i % 3) === 0) {
-        output +='Fizz';
+function fizzbuzz(max) {
+    for (var i = 1; i <= max; i++) {
+        var output = ''
+        if ((i % 3) === 0) {
+            output +='Fizz';
+        }
+        if ((i % 5) === 0) {
+            output += 'Buzz';
+        }
+        if (output === '') {
+            output +=i;
+        }
+        console.log(output);
     }
-    if ((i % 5) === 0) {
-        output += 'Buzz';
-    }
-    if (output === '') {
-        output +=i;
-    }
-    console.log(output)
 }
+
+fizzbuzz(5);
+fizzbuzz(13);
+fizzbuzz(89);
+fizzbuzz(233);
