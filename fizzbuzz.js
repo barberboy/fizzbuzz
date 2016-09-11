2cc2fc8af5cf18b6175e648b34213a5f4196124f
Ben Barber <barberboy@gmail.com>
2016-09-11 22:03:59 +0000

    Reduced duplication
    By appending to a string instead of using console.log every time,
    we were able to reduce duplication and have simpler logic.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,11 +1,13 @@
 for (var i = 1; i <= 100; i++) {
-    if( ((i % 3) === 0) && ((i % 5) === 0)) {
-        console.log('FizzBuzz');
-    } else if((i % 3) === 0) {
-        console.log('Fizz');
-    } else if ((i % 5) === 0) {
-        console.log('Buzz');
-    } else {
-        console.log(i);
+    var output = ''
+    if((i % 3) === 0) {
+        output +='Fizz';
     }
+    if ((i % 5) === 0) {
+        output += 'Buzz';
+    }
+    if (output === '') {
+        output +=i;
+    }
+    console.log(output)
 }
